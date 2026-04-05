e6d9a139e5f17a736e44e51961d1c4b5875bca97
dungnt2911 <tiendungcutemetri@gmail.com>
2026-04-05 09:49:09 +0700

    Update category.js

--- backend/src/models/category.js
+++ backend/src/models/category.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const CategorySchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  slug: { type: String, unique: true }, // Thêm trường này để hiển thị và làm URL đẹp
+  slug: { type: String, unique: true }, 
   description: String,
-  status: { type: String, default: 'active' } // Thêm trạng thái ẩn/hiện
+  status: { type: String, default: 'active' } 
 }, { timestamps: true });
 
 module.exports = mongoose.model('Category', CategorySchema);
